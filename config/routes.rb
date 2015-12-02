6b29fb7e285b73ba02448f4f5e1879c9800b3d83
Haris Sibtain <haris.sibtain@gmail.com>
2015-12-01 21:52:06 -0500

    creating home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -48,9 +48,10 @@ Ruby::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your site routed with "root
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'pages#home'
+   get "about" => "pages#about"
 
   # See how all your routes lay out with "rake routes"
 
